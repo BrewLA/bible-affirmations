a455f68591dd2255fa4fb22962dc75c6c2e81cd6
BrewLA <brewla.design@gmail.com>
2024-06-08 23:22:57 -0700

    responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,28 +57,28 @@ function Index() {
   };
 
   return (
-    <main className="flex flex-col gap-8 justify-start items-center">
-      <h1 className="text-[15rem] text-secondary">Affirmations</h1>
-      <form onSubmit={handleSubmit} className="flex justify-center items-center relative">
+    <main className="flex flex-col gap-8 justify-start items-center h-auto px-4 sm:px-8 md:px-16 lg:px-32 xl:px-64">
+      <h1 className="text-[6rem] sm:text-[4rem] md:text-[5rem] lg:text-[10rem] xl:text-[15rem] text-secondary text-center relative z-10">Affirmations</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row justify-center items-center relative z-10 w-full max-w-[64rem] h-auto">
         <input
           type="text"
           value={feeling}
           onChange={(e) => setFeeling(e.target.value)}
           placeholder="What's on your mind?"
-          className="min-w-[40rem] px-4 py-4 placeholder:text-gray-500 bg-white rounded-full pr-14 focus:outline-none"
+          className="w-full sm:min-w-[30rem] md:min-w-[40rem] px-4 py-4 placeholder:text-gray-500 bg-white rounded-full pr-14 focus:outline-none mb-4 sm:mb-0"
         />
-        <button type="submit" className="absolute w-[1.5rem] h-[1.5rem] right-4">
+        <button type="submit" className="absolute right-4 sm:absolute sm:ml-4 w-[1.5rem] h-[1.5rem]">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><g data-name="92-Arrow Right"><path d="M16 32a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm0-30a14 14 0 1 0 14 14A14 14 0 0 0 16 2z"/><path d="M13.71 24.71 12.3 23.3l7.29-7.3-7.3-7.29L13.7 7.3l8 8a1 1 0 0 1 0 1.41z"/></g></svg>
         </button>
       </form>
       {error && (
-        <div className="text-red-500 bg-white p-4 rounded-2xl">
+        <div className="text-red-500 bg-white p-4 rounded-2xl max-w-[64rem]">
           {error}
         </div>
       )}
       {verse && (
-        <div className="max-w-[64rem] border border-white p-12 rounded-[4rem] relative z-50">
-          <h2 className="text-center text-primary text-[4rem] leading-[3.8rem]">
+        <div className="max-w-[64rem] h-auto border border-white p-12 rounded-[4rem] relative z-50">
+          <h2 className="text-center text-primary text-3xl sm:text-[2rem] sm:leading-tight md:text-[3rem] lg:text-[4rem] leading-tight">
             {animatedMessage.map((word, index) => (
               <span
                 key={index}
@@ -90,23 +90,23 @@ function Index() {
             ))}
           </h2>
           {responseMessage && (
-            <p className="text-center text-primary text-lg mt-8">{responseMessage}</p>
+            <p className="text-center text-primary text-base sm:text-lg mt-8">{responseMessage}</p>
           )}
         </div>
       )}
-      <ul className="flex flex-row gap-2 p-4 text-primary relative z-10">
+      <ul className="flex flex-col sm:flex-row gap-2 px-4 pt-4 pb-12 text-primary relative z-10 text-center">
         <li>
           <Link href="#" onClick={handleOpenModal}>FAQs</Link>
         </li>
-        |
+        <li>|</li>
         <li>
           <Link href="/about">Join our community!</Link>
         </li>
-        |
+        <li>|</li>
         <li>
           <Link href="/blog/hello-world">View products</Link>
         </li>
-        |
+        <li>|</li>
         <li>
           <Link
             href="https://brewla.design"
@@ -116,17 +116,17 @@ function Index() {
           </Link>
         </li>
       </ul>
-      <div className="flex flex-row w-full absolute bottom-0 z-0">
-        <div className="flex flex-row gap-2 absolute left-[-12rem] bottom-[-2rem] z-0">
+      <div className="flex flex-row w-full fixed bottom-0 z-0 pointer-events-none">
+        <div className="flex flex-row gap-2 fixed left-[-12rem] bottom-[-2rem] z-0">
           <LinearCircle />
         </div>
-        <div className="flex flex-row gap-2 absolute left-[2rem] bottom-[-18rem] z-0">
+        <div className="flex flex-row gap-2 fixed left-[2rem] bottom-[-18rem] z-0">
           <LinearCircle />
         </div>
-        <div className="flex flex-row gap-2 absolute right-[-12rem] bottom-[-2rem] rotate-180 z-0">
+        <div className="flex flex-row gap-2 fixed right-[-12rem] bottom-[-2rem] rotate-180 z-0">
           <LinearCircle />
         </div>
-        <div className="flex flex-row gap-2 absolute right-[2rem] bottom-[-18rem] rotate-180 z-0">
+        <div className="flex flex-row gap-2 fixed right-[2rem] bottom-[-18rem] rotate-180 z-0">
           <LinearCircle />
         </div>
       </div>
