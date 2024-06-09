d09d560a2f99b2b5da1fe093b43f150c8adbc54e
BrewLA <brewla.design@gmail.com>
2024-06-08 18:38:42 -0700

    new index

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,7 @@ function Index() {
       return;
     }
     
-    fetch("http://localhost:8080/api/home", {
+    fetch("https://rashtrq-bible-affirmations.hf.space/api/home", { // Update with your Hugging Face Spaces URL
       method: "POST",
       headers: {
         "Content-Type": "application/json"
@@ -70,7 +70,7 @@ function Index() {
         <button type="submit" className="absolute w-[1.5rem] h-[1.5rem] right-4">
           <svg className="text-black" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
             <g data-name="92-Arrow Right">
-              <path d="M16 32a16 16 0 1 1 16-16 16 16 0 0 1-16 16zm0-30a14 14 0 1 0 14 14A14 14 0 0 0 16 2z"/>
+              <path d="M16 32a16 16 0 1 1 16-16 16 0 0 1-16 16zm0-30a14 14 0 1 0 14 14A14 14 0 0 0 16 2z"/>
               <path d="M13.71 24.71 12.3 23.3l7.29-7.3-7.3-7.29L13.7 7.3l8 8a1 1 0 0 1 0 1.41z"/>
             </g>
           </svg>
